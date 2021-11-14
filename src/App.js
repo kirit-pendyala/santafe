96ff798679745378de9a1f792d450230fe2c51b4
kirit-pendyala <pendyalakirit@gmail.com>
2021-11-14 17:07:15 -0600

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -2,22 +2,41 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const firstName = 'Kirit';
+  const lastName = 'Pendyala';
+  const age = 27;
+  // const getFullName = (firstName, lastName) => {
+  //   return `${firstName} ${lastName}`
+  // }
+  const Biodata = {
+    firstName:  'Kirit',
+    lastName: 'Pendyala',
+    Hobby: 'Cooking, Software',
+    age: 27 
+  }
+
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <h3> Full name : {firstName} {lastName}</h3>
+      <h4> Full name: {`${firstName} ${lastName}`}</h4>
+      <h5> {getFullName('Kirit', 'Pendyalaaa')}</h5>
+      <p> Age : {age}</p> */}
+      <div className="BlogCard">
+        <h3>{Biodata.firstName}</h3>
+        <p>{Biodata.lastName}</p>
+        <hr></hr>
+      </div>
+      <div className="BlogCard">
+        <h3>{Biodata.age}</h3>
+        <p>{Biodata.age}</p>
+        <hr></hr>
+      </div>
+      <div className="BlogCard">
+        <h3>{Biodata.Hobby}</h3>
+        <p>{Biodata.Hobby}</p>
+        <hr></hr>
+      </div>
     </div>
   );
 }
