31e1579232b62848de05f84a3048d603dd47237c
kirit-pendyala <pendyalakirit@gmail.com>
2021-11-17 20:26:25 -0600

    props

--- src/App.js
+++ src/App.js
@@ -32,25 +32,33 @@ class App extends Component {
       firstName:  'Kirit',
       lastName: 'Pendyala',
       Hobby: 'Cooking, Software',
-      age: 27 
+      age: 27 ,
+      likeCount: 30
     },
     {
       id: 2,
       firstName:  'Kirit',
       lastName: 'Pendyala',
       Hobby: 'Cooking, Software',
-      age: 27 
+      age: 27,
+      likeCount: 10
     },
     {
       id: 3,
       firstName:  'Kirit',
       lastName: 'Pendyala',
       Hobby: 'Cooking, Software',
-      age: 27 
+      age: 27,
+      likeCount: 20
     }
   ]
 
 
+
+  onLikeBtnClick = (pos) => {
+    alert('Like Button Clicked at pos =>' + pos);
+  }
+  
   // showBlogs = true;
 
    blogCards = isArrayEmpty(this.biodata) ? [] : this.biodata.map((item, pos) => { 
@@ -58,7 +66,10 @@ class App extends Component {
      
     return (
 
-      <BlogCard key={pos} firstName={item.firstName} lastName={item.lastName} Hobby={item.Hobby}/> 
+      <BlogCard key={pos} firstName={item.firstName} lastName={item.lastName} 
+                          Hobby={item.Hobby} likeCount={item.likeCount} id={item.id}
+                          onLikeBtnClick={this.onLikeBtnClick}
+                          position={pos}/> 
       // <div className="BlogCard" key={item.id}>
       //   <h3>{item.firstName}</h3>
       //   <p>{item.lastName}</p>
@@ -72,14 +83,19 @@ class App extends Component {
     // this.showBlogs = false;
     // this.setState({showBlogs:false});
     let updatedState = !this.state.showBlogs;
-    this.setState({showBlogs: updatedState});
+    // this.setState({showBlogs: updatedState});
+    this.setState((prevState, prevProps) => {
+      return {showBlogs: !prevState.showBlogs}
+    });
     console.log(this.showBlogs);
   }
-  
+
+
   render(){
+    console.log('Render Called');
     return(
       <div className="App">
-      <button onClick={this.onHideBtnClick}>  Hide List </button>
+      <button onClick={this.onHideBtnClick}>  {this.state.showBlogs ? 'Hide List' : 'Show List'} </button>
       <br></br>
       {
         this.state.showBlogs ? this.blogCards : null
--- src/BlogCard.js
+++ src/BlogCard.js
@@ -1,18 +1,63 @@
-import React from 'react'
+// import React, {Component} from 'react';
+import React from 'react';
 import { dumpLogs } from './Utils'; 
 
 import classes from './BlogCard.module.css';
-const BlogCard = (properties) => {
-    dumpLogs(properties);
-    // console.log(dumpLogs);
-    return (
-        <div className={classes.NewBlogCard}>
-            <h3> { properties.firstName}</h3>
-            <p>{properties.lastName}</p>
-            <p>{properties.Hobby}</p>
-        </div>
+// const BlogCard = (properties) => {
+//     class BlogCard extends Component {
+//     render() {
+//         dumpLogs(this.properties);
+//         // console.log(dumpLogs);
+//         return (
+//             <div className={classes.NewBlogCard}>
+//                 <h3> {this.properties.firstName}</h3>
+//                 <p>{this.properties.lastName}</p>
+//                 <p>{this.properties.Hobby}</p>
+//             </div>
+//         )
+//     }
+// }
 
+//  class BlogCard extends Component {
+//      state = {
+//          likeCount: 0
+//      }
+
+//      onLikeBtnClick = () => {
+//          this.setState((prevState, prevProp) => {
+//              return {likeCount : prevState.likeCount + 1}
+//             });
+//      }
+//      render(){
+//          dumpLogs(this.props);
+//          return(
+//              <div className={classes.NewBlogCard}>
+//                 <h3>{this.props.firstName}</h3>
+//                 <h3>{this.props.lastName}</h3>
+//                 <h3>{this.props.Hobby}</h3>
+//                 <p> Like Count: <span className={classes.likeCount}>{this.state.likeCount}</span></p>
+//                 <button onClick={this.onLikeBtnClick}>Like</button>
+//              </div>
+//          )
+//      }
+//  }
+
+
+const BlogCard = (props) => {
+    dumpLogs(props);
+
+    return(
+        <div className={classes.NewBlogCard}>
+            <h3>{props.firstName}</h3>
+            <p>{props.lastName}</p>
+            <p>{props.age}</p>
+            <p>{props.Hobby}</p>
+
+            <p> Like Count: <span className={classes.LikeCount}> {props.likeCount} </span></p>
+            <button onClick={() =>props.onLikeBtnClick(props.position)}>Like </button>
+        </div>
     )
 }
+
 
 export default BlogCard;
